2a52303686d2a14f98dbcef6c6400d6c29c87ff5
Troy Adair <tra418@lehigh.edu>
2022-04-11 00:10:38 -0400

    Graded Assignment 3

--- Assignment 3.R
+++ Assignment 3.R
@@ -1,10 +1,17 @@
 install.packages("here")
 library(here)
 here("data", "PRESIDENT_precinct_general.csv")
+
+# TAA - There is no Data subfolder in your repo, and you were supposed to read the zip file
+# using read_csv
 library(here)
-read.csv(here("PRESIDENT_precinct_general.csv"))
+read.csv(here("Data/PRESIDENT_precinct_general.csv"))
+# TAA - This isn't how here() works.
+
 # Simple Scatterplot
 attach(PRESIDENT_precinct_general)
+
+# TAA - This can't possibly work
 plot(state_po, votes, main="Scatterplot", 
      xlab="state_po ", ylab="votes ", pch=40)
 
